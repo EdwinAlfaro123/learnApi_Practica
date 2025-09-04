4440777aacf47d5c7b5be0fc6dc0a71f1dda63f5
EdwinAlfaro123 <20210300ricaldone.edu.sv>
2025-09-04 15:44:10 -0600

    login y cookie creado

--- src/main/java/IntegracionBackFront/backfront/Config/Argon2/Argon2Password.java
+++ src/main/java/IntegracionBackFront/backfront/Config/Argon2/Argon2Password.java
@@ -1,5 +1,6 @@
 package IntegracionBackFront.backfront.Config.Argon2;
 
+import com.sun.tools.jconsole.JConsoleContext;
 import de.mkammerer.argon2.Argon2;
 import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.stereotype.Service;
@@ -15,5 +16,9 @@ public class Argon2Password {
 
     public String EncryptPassword(String password){
         return argon2.hash(ITERATIONS, MEMORY, PARALLELISM, password);
+    }
+
+    public boolean VerifyPassword(String hashDB, String contrasena) {
+        return argon2.verify(hashDB, contrasena);
     }
 }
